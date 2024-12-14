869297a81f2724293e12473251adbb4b0aa77198
CavGame <cavalctoretgiafi9@gmail.com>
2024-12-14 12:06:08 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("startBtn").addEventListener("click", function() {
+    alert("Avvio dell'emulatore Android!");
+    // Qui puoi aggiungere il codice per emulare l'interfaccia Android
+});
